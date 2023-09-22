554f44c7ed5c60da4294eb4e2c793afc8b7d3fa1
umbromancer <umbromancer@gmail.com>
2023-09-23 00:36:50 +0100

    feat: playRound function - player against computer

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -3,3 +3,24 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     return moves[choice];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    
+    if (playerSelection === 'rock'     && computerSelection === 'scissors' ||
+        playerSelection === 'scissors' && computerSelection === 'paper' ||
+        playerSelection === 'paper'    && computerSelection === 'rock')
+	return `You Win! ${playerSelection} beats ${computerSelection}`;
+
+    else if (playerSelection === 'rock'     && computerSelection === 'paper' ||
+             playerSelection === 'scissors' && computerSelection === 'rock' ||
+             playerSelection === 'paper'    && computerSelection === 'scissors')
+	return `You Loose! ${computerSelection} beats ${playerSelection}`;
+    else
+	return `Draw! ${playerSelection} vs. ${computerSelection}`;
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
